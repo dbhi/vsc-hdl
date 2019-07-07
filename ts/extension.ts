302c86fd5a061d592ede000ea9a07a3a0cf2c0b8
umarcor <unai.martinezcorral@ehu.eus>
2019-07-07 22:25:10 +0200

    initial commit

---
+++ ts/extension.ts
@@ -0,0 +1,22 @@
+import * as vscode from 'vscode';
+//import { ProjectsProvider } from './hdlProjects';
+//import { ResultsProvider } from './hdlResults';
+
+
+export function activate(context: vscode.ExtensionContext) {
+
+	//vscode.window.registerTreeDataProvider('hdlProjects', new ProjectsProvider());
+	//vscode.window.registerTreeDataProvider('hdlResults', new ResultsProvider());
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+
+export function deactivate() {}
