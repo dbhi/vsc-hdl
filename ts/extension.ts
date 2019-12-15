76f7176557bd984005bb78d7de05a308c368ca9b
umarcor <unai.martinezcorral@ehu.eus>
2019-12-15 11:29:37 +0100

    wip

--- ts/extension.ts
+++ ts/extension.ts
@@ -1,21 +1,30 @@
+'use strict';
+
 import * as vscode from 'vscode';
+
+import { DockerTemplatesProvider } from './dockerTemplates';
 //import { ProjectsProvider } from './hdlProjects';
 //import { ResultsProvider } from './hdlResults';
+import { FileExplorer } from './fileExplorer';
+//import { TestView } from './testView';
 
 
 export function activate(context: vscode.ExtensionContext) {
 
+	vscode.window.registerTreeDataProvider('dockerTemplates', new DockerTemplatesProvider());
 	//vscode.window.registerTreeDataProvider('hdlProjects', new ProjectsProvider());
 	//vscode.window.registerTreeDataProvider('hdlResults', new ResultsProvider());
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	vscode.commands.registerCommand('dockerTemplates.addTemplate', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+	vscode.commands.registerCommand('dockerTemplates.rmTemplate', () => vscode.window.showInformationMessage(`Successfully called remove entry.`));
+	vscode.commands.registerCommand('dockerTemplates.runTemplate', () => vscode.window.showInformationMessage(`Successfully called run entry.`));
+	vscode.commands.registerCommand('dockerTemplates.inspectTemplate', () => vscode.window.showInformationMessage(`Successfully called inspect entry.`));
 
-	context.subscriptions.push(disposable);
+	// Samples of `window.createView`
+	new FileExplorer(context);
+
+	// Test View
+	//new TestView(context);
 }
 
 
